cf1fc331ff97fafff9d5350341aa69852113c619
Beighlor <bmartinez14@ewu.edu>
2023-05-30 15:00:13 -0700

    closet and add_clothes

--- app/src/main/java/com/example/wardrobewizard/closet.java
+++ app/src/main/java/com/example/wardrobewizard/closet.java
@@ -1,26 +1,17 @@
 package com.example.wardrobewizard;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link closet#newInstance} factory method to
- * create an instance of this fragment.
- */
+import androidx.fragment.app.Fragment;
+
 public class closet extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
@@ -28,15 +19,6 @@ public class closet extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment viewCloset.
-     */
-    // TODO: Rename and change types and number of parameters
     public static closet newInstance(String param1, String param2) {
         closet fragment = new closet();
         Bundle args = new Bundle();
@@ -58,7 +40,10 @@ public class closet extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.closet, container, false);
+        View view = inflater.inflate(R.layout.closet, container, false);
+
+        // Your code for displaying the closet items goes here
+
+        return view;
     }
 }
